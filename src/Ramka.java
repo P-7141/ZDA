6954df6bb26fa70ed6adb644478b71c07976e01f
P-7141 <lelekiii@o2.pl>
2019-11-17 11:58:12 +0100

    Initial commit

---
+++ src/Ramka.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Ramka extends JFrame {
+
+    private static int liczbaKlikniecDowolonego = 0;
+    private JButton wcisnietyPoprzednio;
+
+    public static void main(String[] args) {
+        new Ramka();
+    }
+
+    public Ramka() {
+
+        JButton b = new JButton("przycisk b");
+        JButton c = new JButton("przycisk c");
+        JButton d = new JButton("przycisk d ");
+
+        getContentPane().add(b);
+        getContentPane().add(c);
+        getContentPane().add(d);
+
+        Sluchacz s = new Sluchacz();
+        Sluchacz s2 = new Sluchacz();
+        Sluchacz s3 = new Sluchacz();
+
+
+        b.addActionListener(s);
+        c.addActionListener(s2);
+        d.addActionListener(s3);
+
+        setLayout(new FlowLayout());
+        pack();
+        setVisible(true);
+    }
+
+    public class Sluchacz implements ActionListener {
+        private int clickCounter;
+
+        public void actionPerformed(ActionEvent e) {
+            Object o = e.getSource();
+            JButton j = (JButton) o;
+            j.setEnabled(false);
+
+            if (wcisnietyPoprzednio!=null){
+                wcisnietyPoprzednio.setEnabled(true);
+            }
+
+            wcisnietyPoprzednio=j;
+
+            System.out.println("klikniecia przycisku : "+clickCounter);
+            clickCounter++;
+        }
+    }
+}
+
+// doprecyzowanie tekst znika z etykiety z drugiego slajdu ostatnia linijka
